7fb2d3eafef9ae43ab35dc30179a88ebe11bd4ce
dat4567 <dathtcomputer@gmail.com>
2025-04-13 21:52:01 +0700

    merge feature

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const dynamoose = require('dynamoose')
+const dynamoose = require('dynamoose');
 
 if (process.env.NODE_ENV === 'development') {
   // Local DynamoDB configuration
@@ -8,15 +8,15 @@ if (process.env.NODE_ENV === 'development') {
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'local',
     },
     region: process.env.AWS_REGION || 'us-east-1',
-  })
+  });
   dynamoose.aws.ddb.local(
     process.env.DYNAMODB_LOCAL_ENDPOINT || 'http://localhost:8000'
-  )
+  );
   // Disable automatic table creation and updates
-  dynamoose.Table.defaults.set({
-    create: false, // Do not create tables automatically
-    update: false, // Do not update tables automatically
-  })
+  // dynamoose.Table.defaults.set({
+  //   create: false, // Do not create tables automatically
+  //   update: false, // Do not update tables automatically
+  // })
 }
 
-module.exports = dynamoose
+module.exports = dynamoose;
